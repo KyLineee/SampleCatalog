56fc7fd4a207bba6d80e39defcad70756ab9e2a9
Ali <alihuseyni523@gmail.com>
2023-10-03 23:37:02 +0300

    Adding a form

--- app/src/main/java/com/example/catalog/ItemColumn.kt
+++ app/src/main/java/com/example/catalog/ItemColumn.kt
@@ -2,17 +2,29 @@ package com.example.catalog
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,10 +33,18 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.window.Dialog
 
 
 @Composable
 fun ItemColumn(item: ItemColumnModel, navController: NavController) {
+    var showDialog by remember { mutableStateOf(false) }
     Card(
         modifier = Modifier.padding(10.dp),
         shape = RoundedCornerShape(15.dp),
@@ -32,7 +52,7 @@ fun ItemColumn(item: ItemColumnModel, navController: NavController) {
     ) {
         Box(
             modifier = Modifier.background(Color.LightGray)
-        ){
+        ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -46,15 +66,98 @@ fun ItemColumn(item: ItemColumnModel, navController: NavController) {
                 )
                 Text(text = item.title)
                 Text(text = item.descriptionTitle)
+
                 Button(
                     onClick = {
-                              navController.navigate(route = Screen.Order.route)
+                        showDialog = true
                     },
                     colors = ButtonDefaults.buttonColors(colorResource(id = R.color.color_button))
                 ) {
                     Text(text = "Заказать")
                 }
+
+                if (showDialog) {
+                    DialogWithImage(
+                        onDismissRequest = { showDialog = false },
+                        onConfirmation = { showDialog = false }
+                    )
+                }
             }
         }
     }
+}
+
+@Composable
+fun DialogWithImage(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(340.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ){
+                Column (
+                    modifier = Modifier.height(250.dp),
+                    verticalArrangement = Arrangement.Center
+                ){
+                    PhoneField()
+                    EmailField()
+                }
+3
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    TextButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Dismiss")
+                    }
+                    TextButton(
+                        onClick = { onConfirmation() },
+                        modifier = Modifier.padding(8.dp),
+                    ) { Text("Confirm") }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmailField() {
+    var text by remember { mutableStateOf(TextFieldValue()) }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Email:") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        modifier = Modifier.padding(10.dp)
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PhoneField() {
+    var text by remember { mutableStateOf(TextFieldValue()) }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Phone:") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+        modifier = Modifier.padding(10.dp)
+    )
 }
--- app/src/main/java/com/example/catalog/Order.kt
+++ app/src/main/java/com/example/catalog/Order.kt
@@ -3,14 +3,23 @@ package com.example.catalog
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.navigation.NavController
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
 fun Order(
